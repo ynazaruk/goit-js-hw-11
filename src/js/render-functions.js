7853d4b2f953120db8e9b6af65b9027d2b80dbf3
ynazaruk <nazaruk.ya@gmail.com>
2024-05-28 23:12:26 +0300

    2

---
+++ src/js/render-functions.js
@@ -0,0 +1,7 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+function displayImages(images) {
+  const gallery = document.querySelector('.gallery');
+  gallery.innerHTML = '';
+}
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
